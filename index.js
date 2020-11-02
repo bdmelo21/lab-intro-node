7f7d476cf39df6de402e2a6344ece4da1b81590f
Bernardo Melo <bernardodmelo@gmail.com>
2020-11-02 15:00:43 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length= this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort(function(a,b) {
+      return a-b;
+    });
+    this.length= this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos>=this.items.length) {
+      throw new Error ('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length===0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[(this.items.length-1)];
+  }
 
-  min() {}
+  min() {
+    if (this.items.length===0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    let sum=0;
+    this.items.forEach(element => {
+      sum+=element;
+    });
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (this.items.length===0) {
+      throw new Error('EmptySortedList');
+    }
+    let sum=0;
+    this.items.forEach(element => {
+      sum+=element;
+    });
+    return sum/this.length;
+  }
 }
 
 module.exports = SortedList;
